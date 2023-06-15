f9cc9b100c2b9ba6cb37c7847974c8e064082439
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2023-06-15 12:49:15 +0100

    Dockerise pipeline.py, added command-line arg handling, fixed file tracking in git

--- week_1_basics_and_setup/docker_sql/pipeline.py
+++ week_1_basics_and_setup/docker_sql/pipeline.py
@@ -1,10 +1,68 @@
-import sys
+# pipeline.py
+
+import argparse
+import requests
+import time
 import pandas as pd
+from sqlalchemy import create_engine
 
-print(sys.argv)
 
-day = sys.argv[1]
+def main(params):
 
-# pandas actions
+    # pandas actions
+    # engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+    engine = create_engine(f'postgresql://{params.user}:{params.password}@{params.host}:{params.port}/{params.db}')
+    engine.connect()
 
-print(f'Job completed successfully for day - {day}')
+    # Read file from URL and save in cwd
+    response = requests.get(params.url)
+    with open('yellow_tripdata_2021-01-download.parquet', 'wb') as file:
+        file.write(response.content)
+
+    # Read parquet
+    df = pd.read_parquet('yellow_tripdata_2021-01-download.parquet')
+
+    # Convert to csv
+    df.to_csv('yellow_tripdata_2021-01.csv', index=False)
+
+    # Read back into df as an iterator object
+    df_iter = pd.read_csv('yellow_tripdata_2021-01.csv', iterator=True, chunksize=100_000)
+
+    # First 100,000 rows
+    df = next(df_iter)
+
+    # Write column names then first 100k rows
+    df.head(0).to_sql(name=f'{params.tablename}', con=engine, if_exists='replace')
+    df.to_sql(name=f'{params.tablename}', con=engine, if_exists='append')
+
+    # Loop to cycle through the rest and write in chunks
+    # Iterate through the iterator, not next(iterator). The for loop will automatically call next
+    for df in df_iter:
+        start_chunk = time.time()
+        df.to_sql(name=f'{params.tablename}', con=engine, if_exists='append')
+        end_chunk = time.time()
+        print(f'Chunk added to postgres. Time taken: {(end_chunk - start_chunk):.2f}s')
+
+
+    print(f'Job completed successfully')
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    parser = argparse.ArgumentParser(description='Ingest ny_taxi data into postgres database')
+    parser.add_argument('--user', help='username for postgres')
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--host', help='hostname for postgres')
+    parser.add_argument('--port', help='port number for postgres')
+    parser.add_argument('--db', help='name of database')
+    parser.add_argument('--tablename', help='name of the table in postgres db')
+    parser.add_argument('--url', help='The url that the taxi data will be downloaded from')
+    args = parser.parse_args()
+    print(args)
+    print(args.host)
+    main(args)
+
+    end_time = time.time()
+    print(f'Job finished in {(end_time - start_time):.2f}s')
+
--- week_1_basics_and_setup/docker_sql/run_commands.md
+++ week_1_basics_and_setup/docker_sql/run_commands.md
@@ -29,3 +29,79 @@ docker run -it `
 
 # https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page
 
+
+# Access pgcli
+pgcli -h localhost -p 5432 -u root -d ny_taxi
+
+# docker command for running pgadmin
+docker run -it `
+    -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" `
+    -e PGADMIN_DEFAULT_PASSWORD="root" `
+    -p 8080:80 `
+    dpage/pgadmin4
+
+
+
+
+# docker network connect
+
+docker network create pg-network
+
+# name needed in order for pgadmin to discover/connect to postgres
+docker run -it `
+    -e POSTGRES_USER="root" `
+    -e POSTGRES_PASSWORD="root" `
+    -e POSTGRES_DB="ny_taxi" `
+    -v C:\Users\mattc\OneDrive\Documents\Data\data-engineering-zoomcamp\week_1_basics_and_setup\docker_sql\ny_taxi_postgres_data:/var/lib/postgresql/data `
+    -p 5432:5432 `
+    --network=pg-network `
+    --name pg-database `
+    postgres:13
+
+
+# Run each command one at a time. So we create the network, then add postgres container to it. Then finally add pgadmin container to it.
+docker run -it `
+    -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" `
+    -e PGADMIN_DEFAULT_PASSWORD="root" `
+    -p 8080:80 `
+    --network=pg-network `
+    dpage/pgadmin4
+
+
+
+# run our pipeline from command line with argparse enabled
+python .\pipeline.py `
+    --user=root `
+    --password=root `
+    --host=localhost `
+    --port=5432 `
+    --db=ny_taxi `
+    --tablename=yellow_tripdata `
+    --url="https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet"
+
+
+# Build new docker container
+docker build -t taxi_ingest:v001 .
+
+
+
+# 192 means the file downloads from my localhost location
+# URL2 downloads from online location
+URL = "http://192.168.0.38:8000/yellow_tripdata_2021-01.parquet"
+URL2 = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet"
+
+# Run the new docker container, with arguments for argparse
+# --network is a docker command so runs before the image name
+# Then follow with all arguments for the script
+docker run -it `
+    --network=pg-network `
+    taxi_ingest:v001 `
+    --user=root `
+    --password=root `
+    --host=pg-database `
+    --port=5432 `
+    --db=ny_taxi `
+    --tablename=yellow_tripdata `
+    --url="https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet"
+    
+    
